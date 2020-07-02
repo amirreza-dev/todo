f6ec68765fa9cbf0124a940016fca64ab575b4b4
Amirreza <amirreza.kadz@gmail.com>
2020-07-02 22:08:37 +0430

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/todo/precache-manifest.a0a9d064cc313a4ae14be3543d27c818.js"
+  "/todo/precache-manifest.21600351fe3947646186282e12b0986a.js"
 );
 
 self.addEventListener('message', (event) => {
